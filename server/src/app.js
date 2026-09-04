f0c9ae1ea857e3b2f9fe0ccbcd86e09af3d770ce
Gunanandhini V G <gunavg128@gmail.com>
2026-09-04 11:33:52 +0530

    Register language verification API

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,8 @@ import aiRoutes from "./routes/ai.routes.js";
 import conversationRoutes from "./routes/conv.route.js";
 import closeFriendRoutes from "./routes/closeFriend.routes.js";
 import storyHighlightRoutes from "./routes/storyHighlight.routes.js";
+import languageRoutes from "./routes/language.routes.js";
+
 dotenv.config();
 const app = express();
 
@@ -20,29 +22,21 @@ app.use(
       "https://instagram-clone-pink.vercel.app",
     ],
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  "/api/story-highlights",
-  storyHighlightRoutes
-);
+app.use("/api/story-highlights", storyHighlightRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/language", languageRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/stories", storyRoutes);
 app.use("/api/follow", followRoutes);
 app.use("/api/likes", likesRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/conversation", conversationRoutes);
-app.use(
-  "/api/close-friends",
-  closeFriendRoutes
-);
+app.use("/api/close-friends", closeFriendRoutes);
 app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Instagram Clone API is running 🚀",
-  });
+  res.status(200).json({ success: true, message: "Instagram Clone API is running 🚀" });
 });
 export default app;
