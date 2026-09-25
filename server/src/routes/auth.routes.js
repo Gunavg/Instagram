e01ba06d034f2d2fab89b79ca7846937093d6a0b
Gunanandhini V G <gunavg128@gmail.com>
2026-09-25 18:36:26 +0530

    first commit

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,17 +1,15 @@
 import express from "express";
-import {
-  getProfileByUsername,
-  login,
-  me,
-  register,
-} from "../controllers/auth.controller.js";
+import { getProfileByUsername, getLoginHistory, login, me, register, resendLoginOtp, verifyLoginOtp } from "../controllers/auth.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/login/verify", verifyLoginOtp);
+router.post("/login/resend", resendLoginOtp);
 router.get("/me", protect, me);
+router.get("/login-history", protect, getLoginHistory);
 router.get("/:username", protect, getProfileByUsername);
 
 export default router;
