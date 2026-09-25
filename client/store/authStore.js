145a14337d272cbb40fbc5e0b49993cc0fe71e46
Gunanandhini V G <gunavg128@gmail.com>
2026-09-25 19:16:17 +0530

    first commit

--- client/store/authStore.js
+++ client/store/authStore.js
@@ -3,32 +3,46 @@ import { create } from "zustand";
 const useAuthStore = create((set) => ({
   user: null,
   token: null,
+  refreshToken: null,
   loading: false,
-  login: ({ user, token }) => {
-    console.log(user, token);
-    localStorage.setItem("accessToken", token);
+
+  login: ({ user, token, refreshToken }) => {
+    if (token) localStorage.setItem("accessToken", token);
+    if (refreshToken) localStorage.setItem("refreshToken", refreshToken);
     localStorage.setItem("user", JSON.stringify(user));
-    set({ user, token });
+    set({ user, token, refreshToken: refreshToken || localStorage.getItem("refreshToken") });
   },
+
   logout: () => {
     localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
     localStorage.removeItem("user");
 
     set({
       user: null,
       token: null,
+      refreshToken: null,
     });
   },
+
   loadUser: () => {
     const token = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
     const user = localStorage.getItem("user");
+
     if (token && user) {
       set({
         token,
+        refreshToken,
         user: JSON.parse(user),
       });
     }
   },
-  setUser: (user) => set({ user }),
+
+  setUser: (user) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    set({ user });
+  },
 }));
+
 export default useAuthStore;
