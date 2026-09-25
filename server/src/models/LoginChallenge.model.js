83b20b9ed3ae5abaca5c756d0efdd1e2de165428
Gunanandhini V G <gunavg128@gmail.com>
2026-09-25 19:13:28 +0530

    first commit

--- server/src/models/LoginChallenge.model.js
+++ server/src/models/LoginChallenge.model.js
@@ -4,7 +4,7 @@ const loginChallengeSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
     otpHash: { type: String, required: true },
-    expiresAt: { type: Date, required: true, index: true },
+    expiresAt: { type: Date, required: true },
     attempts: { type: Number, default: 0 },
     resendCount: { type: Number, default: 0 },
     lastSentAt: { type: Date, required: true },
